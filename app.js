66674c578ece1a62e205caa6f476c9c31105d27f
viniciusVina <viniciusfirmino47@gmail.com>
2023-11-03 17:51:13 -0300

    add comentarios

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function verificarChute() {
         limparCampo();
     }
 }
-
+//gerar numeros aleatorios
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
